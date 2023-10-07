d222c1005ff38212ff66847e13f4503d41fa35c5
Ivan Dashkov <iadashkov@gmail.com>
2023-10-07 21:19:37 +0900

    Task 3 complete

--- src/main/kotlin/ru/netology/task03/main.kt
+++ src/main/kotlin/ru/netology/task03/main.kt
@@ -1,5 +1,24 @@
 package ru.netology.task03
 
 fun main() {
-    println("")
+    val isRegularCustomer = true
+
+    println("Введите сумму покупки: ")
+    val purchaseAmount = readln().toDouble()
+
+    val discount = if (purchaseAmount in 1_001.0..10_000.0) {
+        purchaseAmount - 100.0
+    } else if (purchaseAmount > 10_000.0) {
+        purchaseAmount * 5 / 100
+    } else {
+        0.0
+    }
+
+    val finalCost = if (!isRegularCustomer) {
+        purchaseAmount - discount
+    } else {
+        purchaseAmount - discount - (purchaseAmount * 1 / 100)
+    }
+    
+    println("Сумма покупки составит ${String.format("%.2f", finalCost)} руб.")
 }
